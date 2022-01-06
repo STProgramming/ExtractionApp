667dfadaf15dcb679930e81954be8853abe080bb
STProgramming <stcorp@outlook.it>
2022-01-06 14:55:01 +0100

    graphic improvement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ExtractionComponent } from './extraction/extraction.component';
-import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-  {path: 'home', pathMatch: 'full', component: HomeComponent},
-  {path: '', pathMatch: 'full', redirectTo: 'home'},
+const routes: Routes = [  
+  {path: '', pathMatch: 'full', redirectTo: 'extract'},
   {path: 'extract', pathMatch: 'full', component: ExtractionComponent}
 ];
 
--- src/app/extraction/extraction.component.ts
+++ src/app/extraction/extraction.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ExtractionData, ExtractionService } from '../services/extraction.service';
 
 @Component({
@@ -12,21 +13,35 @@ export class ExtractionComponent implements OnInit {
   numberExtracted: number = 0;
   buttonPressTime: number = 0;
   isClicked: any;
+  downloadJsonHref: any;
 
-  constructor(private extractionService: ExtractionService) { }
+  constructor(private extractionService: ExtractionService, private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.extraction = this.extractionService.getExtraction();
   }
 
+  generateData(){
+    if(this.buttonPressTime == 47){
+      this.generateJson(); 
+    }
+  }
+
   makeExtraction(){
     this.isClicked = true;
-    let timeoutVar = Math.floor((Math.random() * this.max)+1)
+    let timeoutVar = Math.floor((Math.random() * this.max)+1000);
     setTimeout(()=>{
       this.numberExtracted = this.extractionService.addExtraction();
       this.buttonPressTime ++;
-      this.isClicked = false;      
+      this.isClicked = false;  
+      this.generateData();  
     }, timeoutVar);
+  }
+
+  generateJson(){
+    var JsonText = JSON.stringify(this.extraction);
+    var uri = this.sanitizer.bypassSecurityTrustUrl("data:text/json;charset=UTF-8,"+encodeURIComponent(JsonText));
+    this.downloadJsonHref = uri;
   }
 
 }
